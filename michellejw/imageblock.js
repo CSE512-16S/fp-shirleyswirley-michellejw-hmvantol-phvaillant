9da7a41e510e27ae0f9b60dc4c56d37f32fc09bf
Michelle Weirathmueller <michw@Michelles-iMac.domain>
2016-05-22 20:44:18 -0700

    attempting to add js to bootstrap modal, unsuccessful

---
+++ michellejw/imageblock.js
@@ -0,0 +1,21 @@
+//var w = 720*2,
+//	h = 480;
+
+//var width = .7*w,
+//	height = .7*h;
+
+var svg = d3.select("myModal")
+		.append("svg")
+		.attr("id", "satellite")
+		//.attr("width", width)
+		//.attr("height", height)
+		//.classed("svg-container",true)
+		//.classed("svg-content-responsive", true);
+		//.attr("viewBox", "0 0 " + width + " " + height);
+
+var image = svg.append("image")
+	.attr("xlink:href","flooding.png")
+	//.attr("width", width)
+	//.attr("height", height)
+	.attr("id","flooding")
+	.style("opacity",1);
