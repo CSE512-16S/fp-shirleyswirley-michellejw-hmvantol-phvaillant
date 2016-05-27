ce6d6cb6bbc65b600228524ae5632f011e816331
Helena van Tol <hmvantol@D-128-208-239-117.dhcp4.washington.edu>
2016-05-27 16:03:41 -0700

    Edit python converter

--- hmvantol/css2tab.py
+++ hmvantol/css2tab.py
@@ -14,19 +14,8 @@ with open(sys.argv[1] + ".css", "r") as f:
 			xy = line.strip(";\r\n").strip("            background-position:")
 			x.append(xy.split(" ")[0].strip("px"))
 			y.append(xy.split(" ")[1].strip("px"))
-		if "width" in line and trig1 == 0:
-			w = line.strip("px;\r\n").strip("            width: ")
-			trig1 = 1
-		if "height" in line and trig2 == 0:
-			h = line.strip("px;\r\n").strip("            height: ")
-			trig2 = 1
-		if " background-size" in line:
-			xy_sum = line.strip(";\r\n").strip("                background-size: ")
-			x_sum = xy.split(" ")[0].strip("px")
-			y_sum = xy.split(" ")[1].strip("px")
 
 
 with open("outfile.csv", "a") as outfile:
-	outfile.write("w," + w + "\nh," + h + "\ntotal_w," + x_sum + "\ntotal_h," + y_sum + "\n")
 	for i in range(len(img_name)):
 		outfile.write(img_name[i] + "," + x[i] + "," + y[i] + "\n")
